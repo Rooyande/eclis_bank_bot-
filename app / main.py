93b133f7a0257ee60c9f3279fa4b3a4e2964eaec
moahmmad javad rooyande <mjrooyande@gmail.com>
2026-01-05 15:47:29 +0330

    Update  main.py

--- app / main.py
+++ app / main.py
@@ -1,1 +1,22 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from app.config import BOT_TOKEN, require_env
+from app.services import init_db
+from app.handlers import router
+
+
+async def main():
+    require_env()
+    await init_db()
+
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
